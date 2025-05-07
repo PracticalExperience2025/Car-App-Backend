003ba440042597a83b0834645e8f5bdaee8a7898
Ivaylo Kartev <92308882+IvayloKartev@users.noreply.github.com>
2025-05-07 10:20:16 -0400

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,11 @@ import { CarModule } from './car/car.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { MailerModule } from './mailer/mailer.module';
+import { MailModule } from './mailer/mailer.module';
 import { SchedulerModule } from './scheduler/scheduler.module';
 
 @Module({
-  imports: [CarModule, PrismaModule, AuthModule, UserModule, MailerModule, SchedulerModule],
+  imports: [CarModule, PrismaModule, AuthModule, UserModule, MailModule, SchedulerModule],
   controllers: [AppController],
   providers: [AppService],
 })
